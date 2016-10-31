acb78f87b59859dd37bfa23b7233b891dcc755b6
cg14823 <cg14823@it025965.users.bris.ac.uk>
2016-10-31 13:56:59 +0000

    Makefile

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
+# Makefile
+
 make:
-  g++  face.cpp  /usr/local/opencv-2.4/lib/libopencv_core.so.2.4
-    /usr/local/opencv-2.4/lib/libopencv_highgui.so.2.4
-    /usr/local/opencv-2.4/lib/libopencv_imgproc.so.2.4
-    /usr/local/opencv-2.4/lib/libopencv_objdetect.so. -0 face.exe
+	g++ -o face.exe face.cpp /usr/local/opencv-2.4/lib/libopencv_core.so.2.4 /usr/local/opencv-2.4/lib/libopencv_highgui.so.2.4 /usr/local/opencv-2.4/lib/libopencv_imgproc.so.2.4 /usr/local/opencv-2.4/lib/libopencv_objdetect.so.2.4
 
 clean:
-  rm *.exe
+	rm *.out
+	rm *.exe
+	rm detected.jpg
--- face.cpp
+++ face.cpp
@@ -22,7 +22,7 @@ using namespace cv;
 void detectAndDisplay( Mat frame );
 
 /** Global variables */
-String cascade_name = "cascade.xml";
+String cascade_name = "frontalface.xml";
 CascadeClassifier cascade;
 
 
@@ -54,7 +54,7 @@ void detectAndDisplay( Mat frame )
 	cvtColor( frame, frame_gray, CV_BGR2GRAY );
 	equalizeHist( frame_gray, frame_gray );
 
-	// 2. Perform Viola-Jones Object Detection 
+	// 2. Perform Viola-Jones Object Detection
 	cascade.detectMultiScale( frame_gray, faces, 1.1, 1, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50), Size(500,500) );
 
        // 3. Print number of Faces found
